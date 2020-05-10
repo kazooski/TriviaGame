ee4f2f57d9cec05364be0ac8117f280fa4abe192
noisette <kasusky.music@gmail.com>
2020-05-10 00:51:38 -0700

    created js psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// Harry Potter Trivia
+
+// create instructions upon loading
+// create start button under buttons-box to start playing
+// start timer 15 sec per question
+// show timer in H1 element
+// display first question
+// create four buttons for the four options
+// create attributes for four questions - one should be correct or true
+// if correct, 
+    // remove buttons
+    // show correct font for 1 second
+    // move onto the next question
+//if incorrect,
+    //remove buttons
+    // show incorrect font for  1 second
+    // move onto the next question
+// once complete show score out total
+// also display try again button that will function like the start button
